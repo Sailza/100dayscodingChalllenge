fca3c3ae06940f646c7052a2f9ad1cc1e6f90539
Sailza <97443167+Sailza@users.noreply.github.com>
2022-10-07 22:14:18 +0530

    Update CODE.java

--- DAY 9/Constructor/CODE.java
+++ DAY 9/Constructor/CODE.java
@@ -8,6 +8,37 @@ SOLUTION : THIS PROBLEM IS VERY SIMPLE , I USED CONSTRUCTOR CONCEPT IN JAVA LANG
 
 LOGIC :- 
 	to find area of cirle we use the formula = [ area= (22*r*r)/7;]
+	
+	
+DESCRIPTION :- 
+	
+	 PROPERTIES OF CONSTRUCTOR 
+ 
+1- Constructors must have the same name as the class within which it is defined while it is not necessary for the method in Java.
+2-Constructors do not return any type while method(s) have the return type or void if does not return any value.
+3-Constructors are called only once at the time of Object creation while method(s) can be called any number of times.
+
+
+
+SYNTAX 
+
+class A
+{   
+  .......
+
+  // A Constructor
+  new A () {
+  }
+
+  .......
+}
+
+// We can create an object of the above class
+// using the below statement. This statement
+// calls above constructor.
+A obj = new A(); 
+
+
 
 
 CODE
