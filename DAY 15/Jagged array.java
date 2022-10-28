3a85011bfb387a31de2b2213683406479962a0c2
Sailza <97443167+Sailza@users.noreply.github.com>
2022-10-28 18:45:16 +0530

    Create Jagged array.java

---
+++ DAY 15/Jagged array.java
@@ -0,0 +1,79 @@
+PROBLEM: Jagged Array in Java
+
+
+EXPLANATION 
+A jagged array is an array of arrays such that member arrays can be of different sizes, i.e.,
+we can create a 2-D array but with a variable number of columns in each row. 
+These types of arrays are also known as Jagged arrays. 
+
+
+Sample Output 
+0 
+
+1 2 
+
+3 4 5 
+
+6 7 8 9 
+
+10 11 12 13 14 
+
+
+CODE 
+class Main {
+
+    public static void main(String[] args)
+
+    {
+
+        int r = 5;
+
+ 
+
+        // Declaring 2-D array with 5 rows
+
+        int arr[][] = new int[r][];
+
+ 
+
+        // Creating a 2D array such that first row
+
+        // has 1 element, second row has two
+
+        // elements and so on.
+
+        for (int i = 0; i < arr.length; i++)
+
+            arr[i] = new int[i + 1];
+
+ 
+
+        // Initializing array
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++)
+
+            for (int j = 0; j < arr[i].length; j++)
+
+                arr[i][j] = count++;
+
+ 
+
+        // Displaying the values of 2D Jagged array
+
+        System.out.println("Contents of 2D Jagged Array");
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j < arr[i].length; j++)
+
+                System.out.print(arr[i][j] + " ");
+
+            System.out.println();
+
+        }
+
+    }
+
+}
