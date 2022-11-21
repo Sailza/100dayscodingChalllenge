0fee9435886fa71182872c5fdec7cab5a4fe40c5
SAILZA KUMARI <97443167+Sailza@users.noreply.github.com>
2022-11-21 12:58:21 +0530

    Create Default_Constructor.java

---
+++ DAY 39/Default_Constructor.java
@@ -0,0 +1,23 @@
+PROBLEM: Default constructor in java show null values
+SOLUTION
+
+CODE 
+class Student3{  
+    int id;  
+    String name;  
+    //method to display the value of id and name  
+    void display(){System.out.println(id+" "+name);}  
+      
+    public static void main(String args[]){  
+    //creating objects  
+    Student3 s1=new Student3();  
+    Student3 s2=new Student3();  
+    //displaying values of the object  
+    s1.display();  
+    s2.display();  
+    }  
+    }  
+    
+    OUTPUT 
+    0 null
+    0 null
