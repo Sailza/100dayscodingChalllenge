d83c5d5711b0c9f6ad939f8b7cd67cd2c00117cd
SAILZA KUMARI <97443167+Sailza@users.noreply.github.com>
2022-12-04 12:31:46 +0530

    Create Implement_Multiple_interface_Code.java

---
+++ DAY 52/Implement_Multiple_interface_Code.java
@@ -0,0 +1,33 @@
+PROBLEM : To implement multiple interfaces, separate them with a comma:
+
+SOLUTION 
+CODE 
+interface FirstInterface {
+  public void myMethod(); // interface method
+}
+
+interface SecondInterface {
+  public void myOtherMethod(); // interface method
+}
+
+// DemoClass "implements" FirstInterface and SecondInterface
+class DemoClass implements FirstInterface, SecondInterface {
+  public void myMethod() {
+    System.out.println("Some text..");
+  }
+  public void myOtherMethod() {
+    System.out.println("Some other text...");
+  }
+}
+
+class MyMainClass {
+  public static void main(String[] args) {
+    DemoClass myObj = new DemoClass();
+    myObj.myMethod();
+    myObj.myOtherMethod();
+  }
+}
+
+OUTPUT 
+Some text..
+Some other text...
