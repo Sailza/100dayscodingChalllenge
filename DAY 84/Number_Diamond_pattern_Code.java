83f851a86d18383b0194e4e5427beeba38bc361d
SAILZA KUMARI <97443167+Sailza@users.noreply.github.com>
2023-01-05 19:29:01 +0530

    Create Number_Diamond_pattern_Code.java

---
+++ DAY 84/Number_Diamond_pattern_Code.java
@@ -0,0 +1,52 @@
+PROBLEM : To print Diamond pattern using numbers 
+SOLUTION 
+CODE
+public class Pattern4  
+{              
+public static void main(String[] args)   
+{  
+for (int i = 1; i <= 4; i++)  
+{  
+int n = 8;  
+for (int j = 1; j<= n - i; j++)   
+{   
+System.out.print(" ");   
+}   
+for (int k = i; k >= 1; k--)  
+{  
+System.out.print(k);  
+}  
+for (int l = 2; l <= i; l++)   
+{  
+System.out.print(l);   
+}   
+System.out.println();   
+}   
+for (int i = 3; i >= 1; i--)  
+{  
+int n = 10;  
+for (int j = 0; j<= n - i; j++)   
+{  
+System.out.print(" ");   
+}   
+for (int k = i; k >= 1; k--)  
+{  
+System.out.print(k);  
+}  
+for (int l = 2; l <= i; l++)  
+{  
+System.out.print(l);  
+}  
+System.out.println();  
+}  
+}  
+}  
+
+OUTPUT 
+       1
+      212
+     32123
+    4321234
+     32123
+      212
+       1
